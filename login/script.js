dcef04b615d8c5aa9c28b14cd4ece60960742199
Papa-Gandhi <69589036+Papa-Gandhi@users.noreply.github.com>
2022-03-07 12:25:34 -0500

    Add files via upload

---
+++ login/script.js
@@ -0,0 +1,36 @@
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "PapaGandhi" && password == "SQHn2UuC34w6uGm6"){
+
+window.location = "encrypt"; // Redirecting to other page.
+return false;
+}
+else{
+  if ( username == "Silverdiamond" && password == "HTGXb3HPUD4VNQxS"){
+
+window.location = "encrypt"; // Redirecting to other page.
+return false;
+}
+    
+  else if ( username == "Simplykin" && password == "scbpwYwNAgYGVP4g"){
+
+window.location = "encrypt"; // Redirecting to other page.
+return false;
+    }
+  else if ( username == "Chrisisdumbaf" && password == "8FZnU82Bmv2LyEfa"){
+
+window.location = "encrypt"; // Redirecting to other page.
+return false;
+    }
+  else if ( username == "Julian" && password == "ZNFkF2xY5aFR577y"){
+
+window.location = "encrypt"; // Redirecting to other page.
+return false;
+    }
+
+
+}
+}
